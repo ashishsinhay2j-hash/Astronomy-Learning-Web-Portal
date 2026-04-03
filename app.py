176022d27af2e750a5daf257266a01b22977f3ba
Ashish Kumar Sinha <ashishsinhay2j@gmail.com>
2026-04-03 22:55:46 +0530

    fixed db connection

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import mysql.connector
 app = Flask(__name__)
 app.secret_key = "secret"
 
+import os
+
 db = mysql.connector.connect(
-    host="localhost",
-    user="astro_user",
-    password="1234",
-    database="astronomy_platform"
+    host=os.environ.get("DB_HOST"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASSWORD"),
+    database=os.environ.get("DB_NAME"),
+    port=int(os.environ.get("DB_PORT", 3306))
 )
+
 
 cursor = db.cursor(dictionary=True)
 
