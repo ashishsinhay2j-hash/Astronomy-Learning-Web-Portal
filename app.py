8961db7a4d2bcafc7179d1fa3b65cca292fcd496
Ashish Kumar Sinha <ashishsinhay2j@gmail.com>
2026-04-05 18:38:56 +0530

    updated

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ db_pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **db_conf
 def get_db():
     return db_pool.get_connection()
 
+# ---------------- HOME ROUTE ----------------
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 # ---------------- SECURITY DECORATORS ----------------
 # This fulfills the "Panels" requirement by strictly protecting routes
 def login_required(f):
